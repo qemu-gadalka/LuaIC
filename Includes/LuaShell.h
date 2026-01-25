42aefc1d176ca9d0991ba48cce3772122bd24586
qemu-gadalka <requests567@gmail.com>
2026-01-25 13:30:52 +0200

    Fix ANSI escape codes for Windows 7 using Win32 API

--- Includes/LuaShell.h
+++ Includes/LuaShell.h
@@ -1,6 +1,42 @@
+#ifndef LUASHELL_H
+#define LUASHELL_H
+
 #pragma once
 
 #ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
+    #define WIN32_LEAN_AND_MEAN
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif
+
+#ifdef _WIN32
+    #define WIN32_LEAN_AND_MEAN
+    #include <windows.h>
+
+    static inline void set_color(int color) {
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        if (hConsole != INVALID_HANDLE_VALUE) {
+            SetConsoleTextAttribute(hConsole, (WORD)color);
+        }
+    }
+
+    #define TERM_GREEN  set_color(10)
+    #define TERM_RED    set_color(12)
+    #define TERM_BLUE   set_color(9)
+    #define TERM_YELLOW set_color(14)
+    #define TERM_CYAN   set_color(11)
+    #define TERM_RESET  set_color(7)
+
+#else
+    #include <stdio.h>
+
+    #define TERM_GREEN  printf("\033[1;32m")
+    #define TERM_RED    printf("\033[1;31m")
+    #define TERM_BLUE   printf("\033[1;34m")
+    #define TERM_YELLOW printf("\033[1;33m")
+    #define TERM_CYAN   printf("\033[1;36m")
+    #define TERM_RESET  printf("\033[0m")
+#endif
+
 #endif
--- LuaShell.c
+++ LuaShell.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "LuaIncludes/lua.h"
 #include "LuaIncludes/lauxlib.h"
@@ -24,9 +25,7 @@ static char *read_input(void) {
         }
         buffer[length++] = (char)ch;
     }
-
     if (length == 0 && ch == EOF) { free(buffer); return NULL; }
-
     buffer[length] = '\0';
     return buffer;
 }
@@ -34,8 +33,6 @@ static char *read_input(void) {
 static int l_sleep(lua_State *L) {
     lua_Integer ms = luaL_checkinteger(L, 1);
     if (ms < 0) ms = 0;
-    const lua_Integer MAX_MS = 60 * 1000;
-    if (ms > MAX_MS) ms = MAX_MS;
 #ifdef _WIN32
     Sleep((DWORD)ms);
 #else
@@ -59,48 +56,33 @@ static int l_cls(lua_State *L) {
 static int l_beep(lua_State *L) {
     lua_Integer freq = luaL_checkinteger(L, 1);
     lua_Integer dur  = luaL_checkinteger(L, 2);
-
     if (freq < 37) freq = 37;
     if (freq > 32767) freq = 32767;
-    if (dur < 0) dur = 0;
-    if (dur > 60000) dur = 60000;
-
 #ifdef _WIN32
-    if (!Beep((DWORD)freq, (DWORD)dur)) {
-        fprintf(stderr, "\033[1;31mWARNING:\033[0m Beep failed (freq=%lld dur=%lld)\n",
-                (long long)freq, (long long)dur);
-    }
+    Beep((DWORD)freq, (DWORD)dur);
 #else
-    (void)freq; (void)dur;
-    fprintf(stderr, "\033[1;33mINFO:\033[0m cbeep not implemented on this platform\n");
+    TERM_YELLOW; printf("INFO: "); TERM_RESET;
+    printf("cbeep not implemented on this platform\n");
 #endif
     return 0;
 }
 
 static void Execute(lua_State *L, const char *Code) {
     if (!L || !Code) return;
-    int status = luaL_dostring(L, Code);
-    if (status != LUA_OK) {
+    if (luaL_dostring(L, Code) != LUA_OK) {
         const char *msg = lua_tostring(L, -1);
-        fprintf(stderr, "\033[1;31mLUA ERROR:\033[0m %s\n", msg ? msg : "unknown");
+        TERM_RED; printf("LUA ERROR: "); TERM_RESET;
+        printf("%s\n", msg ? msg : "unknown");
         lua_pop(L, 1);
     }
 }
 
 static int RunFile(lua_State *L, const char *filename) {
     if (!L || !filename) return 1;
-
-    FILE *f = fopen(filename, "rb");
-    if (!f) {
-        fprintf(stderr, "\033[1;31mFATAL ERROR:\033[0m cannot open file '%s'\n", filename);
-        return 1;
-    }
-    fclose(f);
-
-    int status = luaL_dofile(L, filename);
-    if (status != LUA_OK) {
+    if (luaL_dofile(L, filename) != LUA_OK) {
         const char *msg = lua_tostring(L, -1);
-        fprintf(stderr, "\033[1;31mFATAL ERROR:\033[0m %s\n", msg ? msg : "unknown");
+        TERM_RED; printf("FATAL ERROR: "); TERM_RESET;
+        printf("%s\n", msg ? msg : "unknown");
         lua_pop(L, 1);
         return 1;
     }
@@ -110,49 +92,43 @@ static int RunFile(lua_State *L, const char *filename) {
 int main(int argc, char *argv[]) {
 #ifdef _WIN32
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (hOut != INVALID_HANDLE_VALUE) {
-        DWORD dwMode = 0;
-        if (GetConsoleMode(hOut, &dwMode)) {
-            SetConsoleMode(hOut, dwMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
-        }
+    DWORD dwMode = 0;
+    if (hOut != INVALID_HANDLE_VALUE && GetConsoleMode(hOut, &dwMode)) {
+        SetConsoleMode(hOut, dwMode | 0x0004);
     }
 #endif
 
     setvbuf(stdout, NULL, _IONBF, 0);
 
     if (argc > 1 && strcmp(argv[1], "--help") == 0) {
-        printf("\033[1;32mLuaIC - Lua In C\033[0m\n");
-        printf("2026 - 2026\n");
-        printf("By @LostArduino (LostMind)\n");
+        TERM_GREEN; printf("LuaIC - Lua In C\n"); TERM_RESET;
+        printf("2026 | By @LostArduino\n");
         printf("Usage: luaic [file.lua]\n");
         return 0;
     }
 
     lua_State *L = luaL_newstate();
-    if (!L) {
-        fprintf(stderr, "\033[1;31mFATAL ERROR:\033[0m cannot create Lua state: not enough memory\n");
-        return 1;
-    }
-
+    if (!L) return 1;
     luaL_openlibs(L);
+
     lua_register(L, "ccls", l_cls);
     lua_register(L, "csleep", l_sleep);
     lua_register(L, "cbeep",  l_beep);
 
-    int exit_code = 0;
     if (argc > 1) {
-        if (RunFile(L, argv[1]) != 0) {
-            exit_code = 1;
-        }
+        if (RunFile(L, argv[1]) != 0) return 1;
     } else {
-        printf("\033[1;32mLuaIC 1.2\033[0m | \033[1;34mShell\033[0m | \033[1;33mLua 5.5\033[0m\n");
-        printf("Built-in: \033[1;36mcsleep(ms)\033[0m, \033[1;36mcbeep(f, d)\033[0m\n");
-        printf("Usage: luaic [file.lua] or type 'exit()'\n");
+        TERM_GREEN; printf("LuaIC 1.2 "); TERM_RESET;
+        printf("| ");
+        TERM_BLUE; printf("Shell "); TERM_RESET;
+        printf("| ");
+        TERM_YELLOW; printf("Lua 5.x\n"); TERM_RESET;
 
         while (1) {
-            printf("\033[1;33m> \033[0m");
+            TERM_YELLOW; printf("> "); TERM_RESET;
             char *input = read_input();
             if (!input) break;
+            
             if (strcmp(input, "exit()") == 0 || strcmp(input, "quit()") == 0) {
                 free(input);
                 break;
@@ -163,5 +139,5 @@ int main(int argc, char *argv[]) {
     }
 
     lua_close(L);
-    return exit_code;
+    return 0;
 }
